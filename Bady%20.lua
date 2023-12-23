511db79032b1cc924bc53f94eeba0d0d0b8261b1
AGENTIR006 <148233435+AGENTIR006@users.noreply.github.com>
2023-12-23 16:07:14 +0300

    Create Bady%20.lua

---
+++ Bady%20.lua
@@ -0,0 +1,6 @@
+https://raw.githubusercontent.com/slf0Dev/Ocerium_Project/main/Library.luaWindow = Library.Main("Your Text","LeftAlt") -- change "LeftAlt" to key that you want will hide gui--//tab
+local Tab = Window.NewTab("Your Tab Text")
+
+
+--//section
+local Section = Tab.NewSection("Section Text")
