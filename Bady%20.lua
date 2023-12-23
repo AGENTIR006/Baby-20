30c5be21de9a53160a7d6c565401a64fb713b346
AGENTIR006 <148233435+AGENTIR006@users.noreply.github.com>
2023-12-23 16:08:03 +0300

    Update Bady%20.lua

--- Bady%20.lua
+++ Bady%20.lua
@@ -4,3 +4,23 @@ local Tab = Window.NewTab("Your Tab Text")
 
 --//section
 local Section = Tab.NewSection("Section Text")
+--// Button
+local Button = Section.NewButton("Button Text",function()
+-- code here
+end)
+
+
+--//Toggles
+local EnabledToggle = Section.NewToggle("Enabled Toggle",function(bool)
+-- code here
+end,true) -- "true" is the default value of toggle
+
+local DisabledToggle = Section.NewToggle("Disabled Toggle",function(bool)
+-- code here
+end,false) -- "false" is the default value of toggle
+local SliderPrecise = section.NewSlider("Slider precise",0,100,true,function(value)
+
+end,25)
+local SliderNotPrecise = section.NewSlider("Slider not precise",0,100,false,function(value)
+
+end,75)
